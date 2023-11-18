e43f73ad76e8b02df73f00275279628b1c99973e
HOEVENAERS Eddy <eddy.hoevenaers@ext.ec.europa.eu>
2023-11-18 21:08:41 +0100

    Fixed Fixed same site attribute

--- src/main/java/com/sii/eucaptcha/EuCaptchaApplication.java
+++ src/main/java/com/sii/eucaptcha/EuCaptchaApplication.java
@@ -5,7 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.MapSessionRepository;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
+import java.util.concurrent.ConcurrentHashMap;
+
+@EnableSpringHttpSession
 @SpringBootApplication
 @Slf4j
 public class EuCaptchaApplication extends SpringBootServletInitializer {
@@ -16,5 +24,21 @@ public class EuCaptchaApplication extends SpringBootServletInitializer {
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(EuCaptchaApplication.class, args);
+	}
+
+	@Bean
+	CookieSerializer cookieSerializer() {
+		DefaultCookieSerializer defaultCookieSerializer = new DefaultCookieSerializer();
+		defaultCookieSerializer.setCookieName("JSESSIONID");
+		defaultCookieSerializer.setUseHttpOnlyCookie(true);
+		defaultCookieSerializer.setCookiePath("/");
+		defaultCookieSerializer.setUseSecureCookie(true);
+		defaultCookieSerializer.setSameSite("none");
+		return defaultCookieSerializer;
+	}
+
+	@Bean
+	public MapSessionRepository sessionRepository() {
+		return new MapSessionRepository(new ConcurrentHashMap<>());
 	}
 }
